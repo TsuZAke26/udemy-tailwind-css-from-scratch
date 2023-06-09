e9fa3466ed2151808fbacb490a7d9fcc9093c7bf
TsuZAke26 <ricky.sanders62@gmail.com>
2023-06-09 16:59:48 -0500

    Finishing the Shortly website project

--- website-projects/shortly-website/js/script.js
+++ website-projects/shortly-website/js/script.js
@@ -1,8 +1,19 @@
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
 const input = document.getElementById('link-input');
-const form = document.getElementById('link-form');
+const linkForm = document.getElementById('link-form');
 const errorMessage = document.getElementById('error-message');
 
-form.addEventListener('submit', formSubmit);
+menuBtn.addEventListener('click', navToggle);
+linkForm.addEventListener('submit', formSubmit);
+
+// Toggle Menu button
+function navToggle() {
+	menuBtn.classList.toggle('open');
+	menu.classList.toggle('flex');
+	menu.classList.toggle('hidden');
+}
 
 function validateUrl(value) {
 	return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
